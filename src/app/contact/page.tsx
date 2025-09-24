45970955281921d74649e79872dc8a7be840d8c6
vivek rajendra joge <70814190+BaBu-eng@users.noreply.github.com>
2025-09-24 20:15:58 +0530

    Add files via upload
    Add Contact Page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../Components/Navbar/Navbar'
+import ContactPage from '../Components/ContactPage/ContactPage'
+import Footer from '../Components/Footer/Footer'
+
+const page = () => {
+  return (
+    <div>
+        <Navbar/>
+        <ContactPage/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default page
