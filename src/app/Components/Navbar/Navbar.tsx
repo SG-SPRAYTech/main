8f1f0674106fd0c80cdc720ab73e4e70e690e502
vivek rajendra joge <70814190+BaBu-eng@users.noreply.github.com>
2025-09-26 21:05:45 +0530

    Update Navbar.tsx
    Add prodect file

--- src/app/Components/Navbar/Navbar.tsx
+++ src/app/Components/Navbar/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const navLinks = [
-    { href: '/products', label: 'Products' },
+    { href: '/Product', label: 'Products' },
     { href: '/technologies', label: 'Technologies' },
     { href: '/ContactPage', label: 'About Us' },
   ];
