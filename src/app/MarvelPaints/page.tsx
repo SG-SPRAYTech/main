ec6676ce48cc352b7ac1fad3b2dc81f996bcd52e
vivek rajendra joge <70814190+BaBu-eng@users.noreply.github.com>
2025-10-09 23:40:39 +0530

    Add files via upload
    added SG and Marvel site folders

---
+++ src/app/MarvelPaints/page.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Link from "next/link";
+import Navbar from './Navbar/Navbar';
+import BodyBG from '../Components/Body/BodyBG';
+import Services from '../Components/Services/Services';
+import Contact from '../Components/Contact/Contact';
+import Footer from '../Components/Footer/Footer';
+
+const page = () => {
+  return (
+    <div>
+        <Navbar/>
+        <BodyBG/>
+        <Services/>
+        <Contact/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default page
