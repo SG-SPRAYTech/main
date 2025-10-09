231fda78647bab687a86135af1485085aaec8394
vivek rajendra joge <70814190+BaBu-eng@users.noreply.github.com>
2025-10-10 00:13:52 +0530

    Update page.tsx
    error fixed

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import ContactPage from '../Components/ContactPage/ContactPage'
+import Navbar from '../SGSprayTech/Navbar/Navbar'
+import ContactPage from '../Components/ContactPages/ContactPage'
 import Footer from '../Components/Footer/Footer'
 
 const page = () => {
   return (
     <div>
-        <Navbar/>
         <ContactPage/>
         <Footer/>
     </div>
