25d78da0befe51d5abf3c71de20b18c5d325eda4
vivek rajendra joge <70814190+BaBu-eng@users.noreply.github.com>
2025-10-10 00:11:09 +0530

    Update page.tsx
    error fixed

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Navbar from '../Components/Navbar/Navbar'
 import ContactPage from '../Components/ContactPage/ContactPage'
 import Footer from '../Components/Footer/Footer'
 
