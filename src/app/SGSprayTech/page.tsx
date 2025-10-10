c2caf656d8ef5598fde5aaad2e1f32fb3fa6e2bf
vivek rajendra joge <70814190+BaBu-eng@users.noreply.github.com>
2025-10-11 00:54:32 +0530

    Update page.tsx
    added more components

--- src/app/SGSprayTech/page.tsx
+++ src/app/SGSprayTech/page.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Navbar from './Navbar/Navbar'
+import BodyBG from '../Components/Body/BodyBG'
+import Services from '../Components/Services/Services'
+import Contact from '../Components/Contact/Contact'
+import Footer from '../Components/Footer/Footer'
 
 const page = () => {
   return (
     <div>
         <Navbar/>
+        <BodyBG/>
+        <Services/>
+        <Contact/>
+        <Footer/>
     </div>
   )
 }
