65bd2a95b88f8ddd0186fdf3ee934198a90a777d
vivek rajendra joge <70814190+BaBu-eng@users.noreply.github.com>
2025-10-11 00:49:22 +0530

    Update page.tsx
    reomve components

--- src/app/MarvelPaints/page.tsx
+++ src/app/MarvelPaints/page.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import Navbar from './Navbar/Navbar';
-import BodyBG from '../Components/Body/BodyBG';
-import Services from '../Components/Services/Services';
 import Contact from '../Components/Contact/Contact';
 import Footer from '../Components/Footer/Footer';
 
@@ -9,8 +7,6 @@ const page = () => {
   return (
     <div>
         <Navbar/>
-        <BodyBG/>
-        <Services/>
         <Contact/>
         <Footer/>
     </div>
